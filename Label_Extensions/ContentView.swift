2567575347a6ff3d0420eaf1a2b68ba0288bcfe0
Geri Borbás <geri@eppz.eu>
2020-12-21 20:15:24 +0100

    Initial Commit

---
+++ Label_Extensions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Label_Extensions
+//
+//  Created by Geri Borbás on 21/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
